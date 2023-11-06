16a5957f255afc01227f32ee3bfd11652de2205c
Doni Hadimas <donihadimasaprilian@gmail.com>
2023-11-07 06:23:00 +0700

    update: move variable constant

--- src/utils/helper/Constant.tsx
+++ src/utils/helper/Constant.tsx
@@ -1,3 +1,6 @@
+export const USERNAME = 'donihadimas';
+export const SKACCESSTOKEN =
+  'sk.eyJ1IjoiZG9uaWhhZGltYXMiLCJhIjoiY2xvNzFiMnRoMDFnYzJsanVudHljNTEydiJ9.4fDg-AKHZDiXxrL4mKe3Sw';
 export const ACCESSTOKEN =
   'pk.eyJ1IjoiZG9uaWhhZGltYXMiLCJhIjoiY2xvNnprNGt3MDByeTJsbzBhOHc5ejJmbSJ9.G45CMiNJIHNETbB-x_gXIw';
 export const DATASET_ID = 'clogeca6r1cti2amuoxbguf6h';
