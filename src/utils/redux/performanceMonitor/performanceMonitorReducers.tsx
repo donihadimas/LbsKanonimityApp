ce16d1a73c62761b545814f0e63d64ff23886e31
Doni Hadimas <donihadimasaprilian@gmail.com>
2023-12-07 16:04:02 +0700

    update: create glob state KAnonymity Analisys

--- src/utils/redux/performanceMonitor/performanceMonitorReducers.tsx
+++ src/utils/redux/performanceMonitor/performanceMonitorReducers.tsx
@@ -5,11 +5,13 @@ import type {PayloadAction} from '@reduxjs/toolkit';
 export interface PerformanceMonitor {
   rTimeCreateGeofence: Array<any | never>;
   rTimeNotifyInsideGeofencing: Array<any | never>;
+  rTimeImplementingKAnonymity: Array<any | never>;
 }
 
 const initialState: PerformanceMonitor = {
   rTimeCreateGeofence: [],
   rTimeNotifyInsideGeofencing: [],
+  rTimeImplementingKAnonymity: [],
 };
 
 export const PerfMonitorSlice = createSlice({
@@ -40,6 +42,18 @@ export const PerfMonitorSlice = createSlice({
     clearRTimeNotifyInsideGeofencing: (state, action: PayloadAction<any>) => {
       state.rTimeNotifyInsideGeofencing = [];
     },
+    setRTimeImplementingKAnonymity: (state, action: PayloadAction<any>) => {
+      state.rTimeImplementingKAnonymity = [action.payload];
+    },
+    updateRTimeImplementingKAnonymity: (state, action: PayloadAction<any>) => {
+      state.rTimeImplementingKAnonymity = [
+        ...state.rTimeImplementingKAnonymity,
+        action.payload,
+      ];
+    },
+    clearRTimeImplementingKAnonymity: (state, action: PayloadAction<any>) => {
+      state.rTimeImplementingKAnonymity = [];
+    },
   },
 });
 
@@ -50,5 +64,8 @@ export const {
   setRTimeNotifyInsideGeofencing,
   updateRTimeNotifyInsideGeofencing,
   clearRTimeNotifyInsideGeofencing,
+  setRTimeImplementingKAnonymity,
+  updateRTimeImplementingKAnonymity,
+  clearRTimeImplementingKAnonymity,
 } = PerfMonitorSlice.actions;
 export default PerfMonitorSlice.reducer;
